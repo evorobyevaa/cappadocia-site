43baf28be8d977d201f49f1a7461ab1ad0bcddba
Evgeniya Vorobyeva <82096483+evorobyevaa@users.noreply.github.com>
2022-04-13 10:21:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function() {
+  $('.fairytale__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow_prev.svg" alt="" /></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow_next.svg" alt="" /></button>',
+    autoplay: true,
+    fade: true
+  });
+
+  $('.trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow_prev.svg" alt="" /></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow_next.svg" alt="" /></button>',
+    autoplay: true,
+    fade: true
+  });
+ 
+
+  
+});
